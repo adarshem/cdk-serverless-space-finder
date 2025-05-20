f9f9c36eefdde18fd594fea784e14c4a2b36aeaa
adarshem <adarshem@gmail.com>
2025-05-20 10:01:44 -0700

    Created a dynamoDB table

--- src/services/hello.js
+++ src/services/hello.js
@@ -1,6 +1,8 @@
 exports.handler = async (event) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Hello, world!" }),
-    };
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Hello, world! - read from ${process.env.TABLE_NAME}`
+    })
+  };
 };
