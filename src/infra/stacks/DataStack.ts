173a8cc85c6b4a61e04930a85ab03790d0a0748a
adarshem <adarshem@gmail.com>
2025-05-20 10:02:30 -0700

    updated DataStack

--- src/infra/stacks/DataStack.ts
+++ src/infra/stacks/DataStack.ts
@@ -1,10 +1,26 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import {
+  AttributeType,
+  Table as DynamoTable,
+  ITable
+} from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
+import { getSuffixFromStack } from '../../infra/utils';
 
 export class DataStack extends Stack {
+  public readonly spacesTable: ITable;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    const stackSuffix = getSuffixFromStack(this);
+    const dynamoTable = new DynamoTable(this, 'SpacesTable', {
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING
+      },
+      tableName: `SpacesTable-${stackSuffix}`
+    });
 
-    // Define your data stack resources here
+    this.spacesTable = dynamoTable;
   }
 }
